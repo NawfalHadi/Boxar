3ba48246275ea6f8dc388318f1bde10316712ed8
NawfalHadi <nawfalhdi@gmail.com>
2024-04-01 11:27:35 +0800

    create : add 2 section on the main menu

--- app/init.py
+++ app/init.py
@@ -1,9 +1,12 @@
 import pygame
 
+SCREEN_WIDTH = 1366
+SCREEN_HEIGHT = 768
+
 def initialize_game():
     pygame.init()
 
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('My First Pygame App')
 
     return screen
--- app/main.py
+++ app/main.py
@@ -2,9 +2,22 @@ import pygame
 import sys
 
 # === CLASS === #
+import init
 from init import initialize_game
 
+# DEFINE VARIABLE
+LEFT_PANEL_WIDTH = int(init.SCREEN_WIDTH * 35 / 100)
+RIGHT_PANEL_WIDTH = int(init.SCREEN_HEIGHT * 65 / 100)
+PANEL_HEIGHT = init.SCREEN_HEIGHT
+
 screen = initialize_game()
+
+def left_panel():
+    pygame.draw.rect(screen, (255, 255, 255), (LEFT_PANEL_WIDTH // 6 + 1, PANEL_HEIGHT // 6 + 1, LEFT_PANEL_WIDTH // 1.5 - 2, PANEL_HEIGHT // 1.5 - 2))
+    pygame.draw.rect(screen, (0, 0, 0), (LEFT_PANEL_WIDTH // 6, PANEL_HEIGHT // 6, LEFT_PANEL_WIDTH // 1.5, PANEL_HEIGHT // 1.5), 5)  # Border
+
+def right_panel():
+    pygame.draw.rect(screen, (0, 255, 0), (LEFT_PANEL_WIDTH + RIGHT_PANEL_WIDTH // 4, PANEL_HEIGHT // 4, RIGHT_PANEL_WIDTH // 2, PANEL_HEIGHT // 2))
 
 
 running = True
@@ -13,5 +26,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.fill((255, 255, 255))
+
+    # Panel Drawwing
+    pygame.draw.rect(screen, (255,255,255), (0, 0, LEFT_PANEL_WIDTH, init.SCREEN_HEIGHT))
+    left_panel()
+    
+    pygame.draw.rect(screen, (255,255,255), (LEFT_PANEL_WIDTH, 0, RIGHT_PANEL_WIDTH, init.SCREEN_HEIGHT))
+    right_panel()
 
     pygame.display.flip()
